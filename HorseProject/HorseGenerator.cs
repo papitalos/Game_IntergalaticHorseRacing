44e7beddc162dcfe05be694bb067d26455a4527d
ShinWonRyu <118980843+sktt1soul@users.noreply.github.com>
2022-12-15 08:42:44 +0000

    Add files via upload
    Alguem que veja se querem aproveitar alguma coisa daqui.
    Basicamente é uma cena de  geracao automatica estatisticas e nomes de um numero de cavalos (neste caso 10) mas pode ser facilmente alterado
    Tbm fiz com que cavalos de idade de 2 a 4 que é o suposto pico de performance tivessem 15% mais algumas stats.
    Se quiserem aproveitar alguma coisa tem que ver o porque de a aceleracao n estar a a aparecer direito, tentei mudar para double mas n estava a conseguir

---
+++ HorseProject/HorseGenerator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HorseStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var horses = new List<Horse>();
+
+            // List of possible first and last names for the horses
+            var firstNames = new List<string> { "Thunder", "Lightning", "Shadow", "Storm", "Echo" };
+            var lastNames = new List<string> { "Runner", "Charger", "Hoof", "Mane", "Tail" };
+
+            for (float i = 0; i < 10; i++)
+            {
+                // Generate random stats for each horse
+                var age = random.Next(1, 8);
+                var stamina = random.Next(700, 1000);
+                var mass = random.Next(500, 1000);
+                float acceleration = random.Next((int) 0.40, (int) 0.5);
+                var topSpeed = random.Next(13, 16);
+
+                // Choose a random first and last name for the horse
+                var firstName = firstNames[random.Next(firstNames.Count)];
+                var lastName = lastNames[random.Next(lastNames.Count)];
+
+                // Create a new Horse object with the random stats and name
+                var horse = new Horse(age, stamina, mass, acceleration, topSpeed, firstName, lastName);
+
+                // If the horse is between the ages of 2 and 4, increase its stamina, acceleration, and top speed by 15%
+                if (horse.Age >= 2 && horse.Age <= 4)
+                {
+                    horse.IncreaseStats();
+                }
+
+                // Add the horse to the list of horses
+                horses.Add(horse);
+            }
+
+            // Print out the stats for each horse
+            foreach (var horse in horses)
+            {
+                Console.WriteLine(horse);
+            }
+        }
+    }
+
+    class Horse
+    {
+        public int Age { get; set; }
+        public int Stamina { get; set; }
+        public int Mass { get; set; }
+        public float Acceleration { get; set; }
+        public int TopSpeed { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Horse(int age, int stamina, int mass, float acceleration, int topSpeed, string firstName, string lastName)
+        {
+            Age = age;
+            Stamina = stamina;
+            Mass = mass;
+            Acceleration = acceleration;
+            TopSpeed = topSpeed;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        // Increase the horse's stamina, acceleration, and top speed by 15%
+        public void IncreaseStats()
+        {
+            Stamina += (int)(Stamina * 0.15);
+            Acceleration += (int)(Acceleration * 0.15);
+            TopSpeed += (int)(TopSpeed * 0.15);
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}, Age: {Age}, Stamina: {Stamina}, Mass: {Mass}, Acceleration: {Acceleration}, Top Speed: {TopSpeed}";
+        }
+    }
+}
