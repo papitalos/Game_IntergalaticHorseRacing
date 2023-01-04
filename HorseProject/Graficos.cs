9b8879bed064ae751afd9df828c3c93acf9837b4
Marco0Costa <marcoemanuelcst11@gmail.com>
2023-01-04 11:41:45 +0000

    - adicionei p no grsfivod

--- HorseProject/Graficos.cs
+++ HorseProject/Graficos.cs
@@ -710,7 +710,7 @@ namespace HorseProject
                                   "│-» VOLTAR[TAB]           │ Idade: 02 Anos                       Resistencia  »  ■■■□□□□□□□□□□□ │\r\n" +
                                   "│                         │ Estado: Saudável                           Força  »  ■■■■□□□□□□□□□□ │\r\n" +
                                   "│-» SAIR[ESC]             │                                                                     │\r\n" +
-                                  "│                         │ [C]Comprar                                                          │\r\n" +
+                                  "│                         │ [C]Comprar pppppppppppppppppppp                                                         │\r\n" +
                                   "└─────────────────────────┴─────────────────────────────────────────────────────────────────────┘");
                     break;
             }
